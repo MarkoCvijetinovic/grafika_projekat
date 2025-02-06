f6b292a06573fa4d110dc8a98352791442ec867d
Marko Cvijetinovic <mi22007@alas.matf.bg.ac.rs>
2025-02-06 15:05:18 +0100

    Dodato Sunce i asteroid

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -27,32 +27,9 @@ void main()
 out vec4 FragColor;
 
 in vec2 TexCoords;
-in vec3 Normal;
-in vec3 FragPos;
 
-uniform sampler2D texture_diffuse;
-uniform sampler2D texture_specular;
-uniform vec3 lightPos;
-uniform vec3 lightColor;
-uniform vec3 viewPos;
+uniform sampler2D texture_diffuse1;
 
 void main() {
-
-    //ambient
-    float ambient_strength = 0.1;
-    vec3 ambient = ambient_strength * lightColor * texture(texture_diffuse, TexCoords).rgb;
-
-    //diffuse
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0);
-    vec3 diffuse = diff * lightColor * texture(texture_diffuse, TexCoords).rgb;
-
-    //specular
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = spec * lightColor * texture(texture_specular, TexCoords).rgb;
-
-    FragColor = vec4(ambient + diffuse + specular, 1.0);
+    FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
 }
