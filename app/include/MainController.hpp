9e7fab6701fef829a079c6d22dc6527e856dcc71
Marko Cvijetinovic <mi22007@alas.matf.bg.ac.rs>
2025-02-09 23:49:52 +0100

    Uradjen instancing i dodata jos jedna planeta i svemirski brod

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -18,8 +18,6 @@ private:
 
     void begin_draw() override;
 
-    void draw_skybox();
-
     void draw() override;
 
     void end_draw() override;
@@ -28,11 +26,31 @@ private:
 
     void poll_events() override;
 
-    void draw_backpack();
+    void draw_skybox();
+
+    void draw_phoenix_planet();
+
+    void draw_star();
+
+    void draw_asteroid();
+
+    void draw_spaceship();
+
+    void draw_mars();
 
     void update_camera();
 
     bool cursor_enabled{true};
+
+    void initialize_asteroids();
+
+    unsigned int amount      = 2000;
+    glm::mat4 *modelMatrices = nullptr;
+
+    glm::vec3 starPos   = glm::vec3(-6.0f, 0.0f, -10.0f);
+    glm::vec3 starColor = glm::vec3(1.0f, 0.9f, 0.7f);
+
+    glm::vec3 marsPos = glm::vec3(1.0f, 0.0f, -1.0f);
 };
 
 #endif //MAINCONTROLLER_HPP
--- app/resources/shaders/asteroid.glsl
+++ app/resources/shaders/asteroid.glsl
@@ -4,10 +4,11 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in mat4 aInstanceMatrix;
 
 out vec2 TexCoords;
-out vec3 Normal;
 out vec3 FragPos;
+out vec3 Normal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -15,10 +16,10 @@ uniform mat4 projection;
 
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = aNormal;
+    FragPos = vec3(aInstanceMatrix * vec4(aPos, 1.0));
+    Normal = mat3(aInstanceMatrix) * aNormal;
     TexCoords = aTexCoords;
-    gl_Position = projection * view * vec4(FragPos, 1.0);
+    gl_Position = projection * view * aInstanceMatrix * vec4(aPos, 1.0);
 }
 
 //#shader fragment
@@ -27,9 +28,23 @@ void main()
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
 
 uniform sampler2D texture_diffuse1;
+uniform vec3 lightPos;
+uniform vec3 lightColor;
 
 void main() {
-    FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
+    //ambient
+    float ambient_strength = 0.1;
+    vec3 ambient = ambient_strength * lightColor * texture(texture_diffuse1, TexCoords).rgb;
+
+    //diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0);
+    vec3 diffuse = diff * lightColor * texture(texture_diffuse1, TexCoords).rgb;
+
+    FragColor = vec4(ambient + diffuse, 1.0);
 }
