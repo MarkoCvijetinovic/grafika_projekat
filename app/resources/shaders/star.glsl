60f5f617c73cdc9fc7767c6f9c11ba4e5966f4b8
Marko Cvijetinovic <mi22007@alas.matf.bg.ac.rs>
2025-02-24 18:15:39 +0100

    Akcija posle x sekundi na zvezdu postavljena

--- app/resources/shaders/star.glsl
+++ app/resources/shaders/star.glsl
@@ -34,10 +34,10 @@ in vec2 TexCoords;
 
 uniform vec3 lightColor;
 uniform sampler2D texture_diffuse1;
+uniform float luminocity;
 
 void main()
 {
-    float luminocity = 1.8;
     FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb * luminocity, 1.0);
     //FragColor = vec4(1.5, 1.2, 1.0, 1.0);
     float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
