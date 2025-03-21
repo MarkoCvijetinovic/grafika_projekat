8d719132cbb614e80b874f6694ffd3a044050238
Marko Cvijetinovic <mi22007@alas.matf.bg.ac.rs>
2025-03-21 23:52:26 +0100

    Revert "Akcija posle x sekundi na zvezdu postavljena"
    This reverts commit 60f5f617c73cdc9fc7767c6f9c11ba4e5966f4b8.

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -7,9 +7,7 @@
 
 #include "MainController.hpp"
 
-#include <future>
-#include <chrono>
-#include <thread>
+#include <libs/assimp/code/AssetLib/3MF/3MFXmlTags.h>
 #include <libs/glad/include/glad/glad.h>
 #include <spdlog/spdlog.h>
 
@@ -135,9 +133,8 @@ void MainController::draw_star() {
     shader->set_mat4("view", graphics->camera()->view_matrix());
     glm::mat4 model = glm::mat4(1.0f);
     model           = translate(model, starPos);
-    model           = scale(model, glm::vec3(starScale));
+    model           = scale(model, glm::vec3(0.6f));
     shader->set_mat4("model", model);
-    shader->set_float("luminocity", starLuminocity);
 
     auto camera = graphics->camera();
     shader->set_vec3("viewPos", camera->Position);
@@ -379,35 +376,15 @@ void MainController::poll_events() {
 
     if (platform->key(engine::platform::KeyId::KEY_J).is_down()) {
         spotLightColor[0] += 0.02f;
-    }
-    if (platform->key(engine::platform::KeyId::KEY_K).is_down()) {
+    } else if (platform->key(engine::platform::KeyId::KEY_K).is_down()) {
         spotLightColor[1] += 0.02f;
-    }
-    if (platform->key(engine::platform::KeyId::KEY_L).is_down()) {
+    } else if (platform->key(engine::platform::KeyId::KEY_L).is_down()) {
         spotLightColor[2] += 0.02f;
-    }
-    if (platform->key(engine::platform::KeyId::KEY_U).is_down()) {
+    } else if (platform->key(engine::platform::KeyId::KEY_U).is_down()) {
         spotLightColor[0] = std::max(spotLightColor[0] - 0.02f, 0.0f);
-    }
-    if (platform->key(engine::platform::KeyId::KEY_O).is_down()) {
+    } else if (platform->key(engine::platform::KeyId::KEY_O).is_down()) {
         spotLightColor[1] = std::max(spotLightColor[1] - 0.02f, 0.0f);
-    }
-    if (platform->key(engine::platform::KeyId::KEY_P).is_down()) {
+    } else if (platform->key(engine::platform::KeyId::KEY_P).is_down()) {
         spotLightColor[2] = std::max(spotLightColor[2] - 0.02f, 0.0f);
     }
-
-    if (platform->key(engine::platform::KeyId::KEY_T).is_down() && !starKeyPressed) {
-        std::thread(&MainController::alter_star, this).detach();
-        starKeyPressed = true;
-    }
-    if (platform->key(engine::platform::KeyId::KEY_T).is_up()) {
-        starKeyPressed = false;
-    }
-}
-
-void MainController::alter_star() {
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    starLuminocity *= 1.5f;
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    starLuminocity /= 1.5f;
 }
